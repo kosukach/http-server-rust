a7a9860999d30ccffd7ef055d30f75aa9357c72f
Konstantin <kosukach@yahoo.com>
2021-06-16 14:44:15 +0300

    basic html responce

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-use std::io::{Read, Write, BufWriter, BufReader, Result};
+use std::fs;
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(stream: TcpStream) {
-    let mut buffer = String::new();
-    let mut reader = BufReader::new(&stream);
-    match reader.read_to_string(&mut buffer) {
-        Ok(buff) => println!("{}, {}", buffer, buff),
-        Err(_) => (),
-    }
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-    let res = b"HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-    let mut writer = BufWriter::new(&stream);
-    match writer.write(res) {
-       Ok(buff) => println!("{}", buff),
-       Err(_) => (),
-    }
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let inhalt = fs::read_to_string("hello.html").unwrap();
+    let response = format! {
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        inhalt.len(),
+        inhalt
+    };
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
-fn main() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        handle_client(stream?);
-    }
+        let stream = stream.unwrap();
 
-    Ok(())
+        println!("Connected to client");
+        handle_client(stream);
+    }
 }
